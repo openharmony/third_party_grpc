grpc/grpc

Replace Map with std::map

src/core/ext/filters/client_channel/lb_policy/xds/xds.cc
@@ -526,8 +526,8 @@ class XdsLb : public LoadBalancingPolicy {
 
    private:
     XdsLb* xds_policy_;
-    Map<RefCountedPtr<XdsLocalityName>, OrphanablePtr<LocalityEntry>,
-        XdsLocalityName::Less>
+    std::map<RefCountedPtr<XdsLocalityName>, OrphanablePtr<LocalityEntry>,
+             XdsLocalityName::Less>
         map_;
   };
 

openharmony/third_party_grpc

src/core/ext/filters/client_channel/lb_policy/xds/xds.cc
@@ -576,8 +576,8 @@ class XdsLb : public LoadBalancingPolicy {
 
       RefCountedPtr<XdsLb> xds_policy_;
 
-      Map<RefCountedPtr<XdsLocalityName>, OrphanablePtr<Locality>,
-          XdsLocalityName::Less>
+      std::map<RefCountedPtr<XdsLocalityName>, OrphanablePtr<Locality>,
+               XdsLocalityName::Less>
           localities_;
       const uint32_t priority_;
       grpc_connectivity_state connectivity_state_ = GRPC_CHANNEL_IDLE;