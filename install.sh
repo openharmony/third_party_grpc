c68f4bd4cd3b8241a620c79cd3403cd119513533
wenlong_12 <wenlong12@huawei.com>
2023-05-04 17:18:06 +0800

    grpc 版本升级      Signed-off-by: wenlong_12 <wenlong12@huawei.com>
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ if [ -d "grpc-1.41.1" ];then
 fi
 tar xvf grpc-1.41.1.tar.gz
 cd $1/grpc-1.41.1
-patch -p1 < $1/src_core_lib_debug.patch --fuzz=0 --no-backup-if-mismatch
-patch -p1 < $1/src_core_lib_iomgr.patch --fuzz=0 --no-backup-if-mismatch
+patch -p1 < $1/src_core_lib_debug.patch
+patch -p1 < $1/src_core_lib_iomgr.patch
 exit 0
