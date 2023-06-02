45f9905ad633a32760baff49f6a53083f2b4ca09
卢韬 <lutao31@huawei.com>
2023-06-02 09:05:39 +0000

    修复多线程构建时概率失败
    Signed-off-by: 卢韬 <lutao31@huawei.com>

--- install.sh
+++ install.sh
@@ -8,12 +8,11 @@
 
 set -e
 cd $1
-if [ -d "grpc" ];then
-    rm -rf grpc
+if [ ! -d "grpc" ];then
+    mkdir grpc
+    tar -zxvf grpc-1.41.1.tar.gz --strip-components 1 -C ./grpc
+    cd $1/grpc
+    patch -p1 < $1/src_core_lib_debug.patch
+    patch -p1 < $1/src_core_lib_iomgr.patch
 fi
-mkdir grpc
-tar -zxvf grpc-1.41.1.tar.gz --strip-components 1 -C ./grpc
-cd $1/grpc
-patch -p1 < $1/src_core_lib_debug.patch
-patch -p1 < $1/src_core_lib_iomgr.patch
 exit 0
