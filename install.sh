6e38d81bd39869f8769de376ec6e2be5a956a1df
wenlong_12 <wenlong12@huawei.com>
2024-08-01 11:28:41 +0800

    fix CVE-2023-44487     Signed-off-by:wenlong12<wenlong12@huawei.com>
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ patch -p1 < $1/src_core_lib_iomgr.patch
 patch -p1 < $1/backport-Ignore-Connection-Aborted-errors-on-accept-29318.patch
 patch -p1 < $1/backport-iomgr-EventEngine-Improve-server-handling-o.patch
 patch -p1 < $1/fix-CVE-2023-33953-add-header-limit.patch
+patch -p1 < $1/fix-CVE-2023-44487.patch
 flock -u 100
 } 100<>$1/lock_file.lock
 exit 0
